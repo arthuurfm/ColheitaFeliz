990f1eb7884bac949c2e997fb9c880d8eed612b8
Arthur Franz <arthurfranzmoura@gmail.com>
2026-05-25 21:09:06 -0300

    window game loop

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,17 @@
 #include <iostream>
 using namespace std;
 
+constexpr int WINDOW_WIDTH = 1280;
+constexpr int WINDOW_HEIGHT = 720;
+constexpr int FPS = 60;
+constexpr int TIME_FRAME_MS = 1000 / FPS;
+
 int main(int agrc, char* agrv[]) {
   // checa se o áudio ou vídeo incializam.
   if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0) {
     cout << "Erro ao inicializar SDL2: " << SDL_GetError() << endl;
     return 1;
   }
-  
-  cout << "SDL2 Inicializado com sucesso!" << endl;
-
-  SDL_version versao;
-  SDL_GetVersion(&versao); // popula 'versao'.
-  cout << "Versão SDL2: " << int(versao.major) << "." << int(versao.minor) << int(versao.patch);
 
   // checa se o PNG (imagem) inicializa.
   if (!IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) {
@@ -25,8 +24,6 @@ int main(int agrc, char* agrv[]) {
     SDL_Quit(); // limpa o SDL da memória.
     return 1;
   }
-  
-  cout << "SDL2_Image (PNG) inicializado com sucesso!" << endl;
 
   // checa se o TTF inicializa.
   if (TTF_Init() == -1) {
@@ -36,8 +33,6 @@ int main(int agrc, char* agrv[]) {
     return 1;
   }
 
-  cout << "SDL2_ttf inicializado com sucesso!" << endl;
-
   // checa se o mixer inicializa. params: frequência, formato, canais e buffer.
   if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048)) {
     cout << "Erro ao inicializar o SDL2_Mixer: " << Mix_GetError() << endl;
@@ -46,21 +41,88 @@ int main(int agrc, char* agrv[]) {
     SDL_Quit(); // limpa o SDL da memória.
     return 1;
   }
+  
+  // Cria a janela do jogo.
+  SDL_Window* window = SDL_CreateWindow(
+    "Colheita Feliz",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    SDL_WINDOW_SHOWN // mostra a janela imediatamente após ser criada.
+  );
 
-  cout << "SDL2_Mixer inicializado com sucesso!" << endl;
+  if (!window) {
+    cout << "Erro ao criar a janela" << SDL_GetError() << endl;
+    Mix_CloseAudio();
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+    return 1;
+  }
 
-  nlohmann::json test;
-  test["jogo"] = "Colheita Feliz";
-  test["version"] = 1;
+  // objeto que cria pixels na janela.
+  SDL_Renderer* renderer = SDL_CreateRenderer(
+    window, 
+    -1,
+    SDL_RENDERER_ACCELERATED || SDL_RENDERER_PRESENTVSYNC
+  );
 
-  cout << "nlohmann_json funcionando!" << endl;
+  if (!renderer) {
+    cout << "Erro ao criar o renderer" << SDL_GetError() << endl;
+    SDL_DestroyWindow(window); // limpa a janela da memória.
+    Mix_CloseAudio();
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+    return 1;
+  }
 
+  bool is_running = true;
+  SDL_Event event; // controla todos os eventos.
+  Uint32 previous_time = SDL_GetTicks(); // pega todo o tempo desde a inicialização.
+  float deltaTime = 0.0f;
+
+  while (is_running) {
+    Uint32 current_time = SDL_GetTicks();
+    deltaTime = (current_time - previous_time) / 1000.0f;
+    previous_time = current_time;
+
+    // se tiver eventos na fila.
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        is_running = false;
+      }
+
+      if (event.type == SDL_KEYDOWN) {
+        if(event.key.keysym.sym == SDLK_ESCAPE) {
+          is_running = false;
+        }
+      }
+    }
+
+    // desenha uma cor.
+    SDL_SetRenderDrawColor(renderer, 34, 139, 34, 255);
+    // preenche a tela com a cor definida.
+    SDL_RenderClear(renderer);
+    // mostra tudo que foi desenhado, evita piscadas.
+    SDL_RenderPresent(renderer);
+  
+    Uint32 frame_time = SDL_GetTicks() - current_time;
+    if (frame_time < TIME_FRAME_MS) {
+      // se os frames estiverem muito rápidos, ele espera/diminui.
+      SDL_Delay(TIME_FRAME_MS - frame_time);
+    }
+  }
+
+  SDL_DestroyRenderer(renderer); // limpa o renderer da memória.
+  SDL_DestroyWindow(window); // limpa a janela da memória.
   Mix_CloseAudio(); // limpa o mixer da memória.
   TTF_Quit(); // limpa o TTF da memória.
   IMG_Quit(); // limpa o IMG da memória.
   SDL_Quit(); // limpa o SDL da memória.
 
-  cout << "\nTudo funcionando. Ambiente pronto!" << test.dump(2) << endl;
+  cout << "Jogo encerrado com sucesso!" << endl;
 
   return 0;
 }
